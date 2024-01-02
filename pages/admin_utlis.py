a6d21ed15a0e4ac74c79fce230a8614bef880323
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-01-02 18:45:21 +0530

    added classification model

--- pages/admin_utlis.py
+++ pages/admin_utlis.py
@@ -7,6 +7,9 @@ from langchain.llms import OpenAI
 import pinecone
 from langchain.vectorstores import Pinecone
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 def read_pdf_data(pdf_file):
     pdf_page = PdfReader(pdf_file)
@@ -36,3 +39,33 @@ def push_to_pinecone(pinecone_environment,pinecone_index_name,embeddings,docs):
     index_name = pinecone_index_name
     index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
     return index
+
+
+# classification model utils
+
+#Read dataset for model creation
+def read_data(data):
+    df = pd.read_csv(data,delimiter=',', header=None)  
+    return df
+
+def get_embeddings():
+    embeddings = OpenAIEmbeddings()
+    # embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    return embeddings
+
+
+def create_embeddings(df,embeddings):
+    df[2] = df[0].apply(lambda x: embeddings.embed_query(x))
+    return df
+
+
+def split_train_test__data(df_sample):
+    sentences_train, sentences_test, labels_train, labels_test = train_test_split(
+    list(df_sample[2]), list(df_sample[1]), test_size=0.25, random_state=0)
+    print(len(sentences_train))
+    return sentences_train, sentences_test, labels_train, labels_test
+
+
+def get_score(svm_classifier,sentences_test,labels_test):
+    score = svm_classifier.score(sentences_test, labels_test)
+    return score
