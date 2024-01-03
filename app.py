f1fe36d2cb8462509c765827227fbfcac9bbe81c
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2024-01-03 17:30:50 +0530

    implemented ticket raising feature

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 from user_utils import *
 
+#Creating session variables
+if 'HR_tickets' not in st.session_state:
+    st.session_state['HR_tickets'] =[]
+if 'IT_tickets' not in st.session_state:
+    st.session_state['IT_tickets'] =[]
+if 'Transport_tickets' not in st.session_state:
+    st.session_state['Transport_tickets'] =[]
+
 def main():
 
     st.header("Automatic Ticket Classification Tool")
@@ -18,6 +26,20 @@ def main():
         st.write(response)
 
         button = st.button("Submit ticket?")
+
+        if button:
+            embeddings = create_embeddings()
+            query_result = embeddings.embed_query(user_input)
+
+            department_value = predict(query_result)
+            st.write("your ticket has been sumbitted to : "+department_value)
+
+            if department_value=="HR":
+                st.session_state['HR_tickets'].append(user_input)
+            elif department_value=="IT":
+                st.session_state['IT_tickets'].append(user_input)
+            else:
+                st.session_state['Transport_tickets'].append(user_input)
 
 
 if __name__ == '__main__':
--- user_utils.py
+++ user_utils.py
@@ -6,7 +6,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-
+import joblib
 
 def pull_from_pinecone(pinecone_environment,pinecone_index_name,embeddings):
 
@@ -20,18 +20,27 @@ def pull_from_pinecone(pinecone_environment,pinecone_index_name,embeddings):
     index = Pinecone.from_existing_index(index_name, embeddings)
     return index
 
+
 def create_embeddings():
     embeddings = OpenAIEmbeddings()
     # embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
     return embeddings
+
 
 def get_similar_docs(index,query,k=2):
 
     similar_docs = index.similarity_search(query, k=k)
     return similar_docs
 
+
 def get_answer(docs,user_input):
     chain = load_qa_chain(OpenAI(), chain_type="stuff")
     with get_openai_callback() as cb:
         response = chain.run(input_documents=docs, question=user_input)
     return response
+
+
+def predict(query_result):
+    Fitmodel = joblib.load('modelsvm.pk1')
+    result=Fitmodel.predict([query_result])
+    return result[0]
